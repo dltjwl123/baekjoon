d910098ad763cc953348e4ebc591d0171fd584e0
dltjwl123@korea.edu <dltjwl123@korea.edu>
2022-04-11 18:17:19 +0900

    https://www.acmicpc.net/problem/1059

--- 소스.cpp
+++ 소스.cpp
@@ -1,86 +1,56 @@
 #include <iostream>
 #include <vector>
-#define ll long long
-#define V vector<vector<ll>>
-#define divider 1000000007
+#include <algorithm>
 using namespace std;
-//행렬을 출력한다.
-void Print(const V& matrix) {
-	for (int i = 0; i < matrix.size(); i++) {
-		for (int j = 0; j < matrix[0].size(); j++) {
-			cout << matrix[i][j] << " ";
-		}
-		cout << "\n";
-	}
-}
-V Multiply(V& a, V& b) {
-	V ans;
-	ll sum;
-	for (int i = 0; i < a.size(); i++) {
-		vector<ll> tmp;
-		for (int j = 0; j < b[0].size(); j++) {
-			sum = 0;
-			for (int k = 0; k < b.size(); k++) {
-				sum += (a[i][k] * b[k][j]) % divider;
+//binary search
+pair<int, int> Search_range(const vector<int>& arr, const int n) {
+	int i = 0, j = arr.size() - 1;
+	int half;
+	while (1) {
+		half = (i + j + 1) / 2;
+		if (arr[half] == n) {
+			if (!half) {
+				//n이 중복이면 이미 S에 n이 있었다는 것이므로 구간을 구할 수 없다.
+				if (arr[half + 1] == n) return make_pair(0, 0);
+				return make_pair(1, n);
 			}
-			tmp.push_back(sum % divider);
+			if (half == arr.size() - 1) {
+				if (arr[half - 1] == n) return make_pair(0, 0);
+				return make_pair(arr[half - 1] + 1, n);
+			}
+			if (arr[half - 1] == n || arr[half + 1] == n) return make_pair(0, 0);
+			return make_pair(arr[half - 1] + 1, arr[half + 1] - 1);
 		}
-		ans.push_back(tmp);
+		if (arr[half] < n) i = half + 1;
+		else j = half - 1;
 	}
-	return ans;
 }
-V Exp(ll e) {
-	V matrix, total;
-	//(1, 1)
-	//(1, 0)
-	for (int i = 0; i < 2; i++) {
-		vector<ll> tmp;
-		for (int j = 0; j < 2; j++) {
-			if (i == 1 && j == 1) tmp.push_back(0);
-			else tmp.push_back(1);
-		}
-		matrix.push_back(tmp);
-	}
-	//단위행렬
-	for (int i = 0; i < matrix.size(); i++) {
-		vector<ll> tmp;
-		for (int j = 0; j < matrix.size(); j++) {
-			if (i == j) tmp.push_back(1);
-			else tmp.push_back(0);
-		}
-		total.push_back(tmp);
-	}
-	while (e > 0) {
-		if (e % 2) total = Multiply(total, matrix);
-		matrix = Multiply(matrix, matrix);
-		e /= 2;
-	}
-	return total;
+int Count(const pair<int, int>& range, const int n) {
+	if (!range.second || range.first == range.second) return 0;
+	//n ~ max 까지는 시작범위는 항상 n이며 끝나는 범위는 max - n개 존재 = more
+	//[10, 11], [10, 12]
+	//min ~ n 까지는 i부터 n 이상의 수 ~ max의 범위 = (more + 1) * less
+	//[9, 10], [9, 11], [9, 12]
+	int less = n - range.first, more = range.second - n;
+	int ans = (more + 1) * less + more;
+	return ans;
 }
 int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
-	//(Fn) =  (1 1)^(n-2)(F2)
-	//(Fn-1)  (1 0)      (F1)
-	ll n;
-	cin >> n;
-	//F1 = F2 = 1
-	if (n == 1 || n == 2) {
-		cout << 1 << "\n";
-		return 0;
-	}
-	V ans = Exp(n - 2);
-	//(F2) = (1)
-	//(F1)   (1)
-	V first;
-	vector<ll> tmp;
-	tmp.push_back(1);
-	first.push_back(tmp);
-	first.push_back(tmp);
 
-	ans = Multiply(ans, first);
-	cout << ans[0][0] << "\n";
+	vector<int> arr;
+	int L, N;
+	cin >> L;
+	while (L--) {
+		cin >> N;
+		arr.push_back(N);
+	}
+	cin >> N;
+	arr.push_back(N); 
+	sort(arr.begin(), arr.end());
+	//N을 S에 넣고 정렬하여 인접한 두 원소를 범위로 삼는다.
+	pair<int, int> range = Search_range(arr, N);
+	cout << Count(range, N) << "\n";
 }
-//커밋 테스트
-//branch 테스트
